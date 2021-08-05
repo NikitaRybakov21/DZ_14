c48c9443ff32b31a92a87fa07868e20fec2a8965
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-08-05 23:19:51 +0300

    DZ14_Lesson14

--- src/test/java/github/JUnitTest.java
+++ src/test/java/github/JUnitTest.java
@@ -5,5 +5,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class JUnitTest {
-
+    @Test
+    public void test1() {
+        int[] arr = {1,2,4,4,2,3,4,1,7};
+        int[] arrTest = {1,7};
+        Assertions.assertArrayEquals(arrTest, Main.test(arr));
+    }
+    @Test
+    public void test2() {
+        int[] arr = {1,1,1,1,1,1,4,1,1};
+        Assertions.assertTrue(Main.test2(arr));
+    }
 }
